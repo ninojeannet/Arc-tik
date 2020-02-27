79af6834a497802727031152d57e27624b41c2c1
Jeannet Nino <nino.jeannet@he-arc.ch>
2020-02-27 14:14:10 +0100

    create polar image

--- arctik.py
+++ arctik.py
@@ -15,18 +15,25 @@ def findCircles(img):
     circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1.2,100)
 
     if circles is not None:
+        radius = 0
+        center = (0,0)
         circles = np.round(circles[0, :]).astype("int")
         for (x, y, r) in circles:
             cv2.circle(img, (x, y), r, (0, 255, 0), 4)
             cv2.rectangle(img, (x - 5, y - 5), (x + 5, y + 5), (255, 0, 0), -1)
+            radius = r
+            center = (x,y)
         cv2.imshow("circles",img)
+        imgSize = (radius,360)
+        polarImg = cv2.warpPolar(img,imgSize,center,radius,flags=0)
+        cv2.imshow("polar",polarImg)
     else:
         print("no clock detected !")
 
 
 
 if __name__ == "__main__":
-    img = cv2.imread("images/hard.jpg")
+    img = cv2.imread("images/mondaine.jpg")
     img = resize(img,700)
     cv2.imshow("base",img)
     findCircles(img)
