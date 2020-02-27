fd72605bb3b5946945f8f40c1c791ee0611c6c5f
Jeannet Nino <nino.jeannet@he-arc.ch>
2020-02-27 13:55:57 +0100

    img resize keeping ratio

--- arctik.py
+++ arctik.py
@@ -1,13 +1,18 @@
 import cv2.cv2 as cv2
 
 
-if __name__ == "__main__":
-    img = cv2.imread("images/medium.png")
-    
-    width = 700
+# Resize the image at the given width keeping the width/height ratio
+def resize(img,width):
     scale_ratio = width / img.shape[1]
     height = int(img.shape[0] * scale_ratio)
     dim = (width, height)
     img = cv2.resize(img,dim)
+    return img
+
+if __name__ == "__main__":
+    img = cv2.imread("images/simple.jpg")
+    img = resize(img,700)
+
     cv2.imshow("test",img)
+
     cv2.waitKey()
