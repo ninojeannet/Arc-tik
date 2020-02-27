2b4c8890dc1c35ead72ea8cd19001888445f56cf
Jeannet Nino <nino.jeannet@he-arc.ch>
2020-02-27 14:06:37 +0100

    find circles and center

--- arctik.py
+++ arctik.py
@@ -1,4 +1,5 @@
 import cv2.cv2 as cv2
+import numpy as np
 
 
 # Resize the image at the given width keeping the width/height ratio
@@ -9,10 +10,27 @@ def resize(img,width):
     img = cv2.resize(img,dim)
     return img
 
-if __name__ == "__main__":
-    img = cv2.imread("images/simple.jpg")
-    img = resize(img,700)
+def findCircles(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1.2,100)
 
-    cv2.imshow("test",img)
+    if circles is not None:
+        circles = np.round(circles[0, :]).astype("int")
+        for (x, y, r) in circles:
+            cv2.circle(img, (x, y), r, (0, 255, 0), 4)
+            cv2.rectangle(img, (x - 5, y - 5), (x + 5, y + 5), (255, 0, 0), -1)
+        cv2.imshow("circles",img)
+    else:
+        print("no clock detected !")
+
+
+
+if __name__ == "__main__":
+    img = cv2.imread("images/hard.jpg")
+    img = resize(img,700)
+    cv2.imshow("base",img)
+    findCircles(img)
+
+
 
     cv2.waitKey()
